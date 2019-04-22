c3592d0abc5cf4b50586ccdeb985b0bfb298e8b5
Mark Christian Navalta <navalta3030@gmail.com>
2019-04-21 23:04:10 -0700

    initial database

---
+++ db_ElkGrove.sql
@@ -0,0 +1,41 @@
+use master;
+
+DROP DATABASE IF EXISTS db_ElkGrove;
+CREATE DATABASE db_ElkGrove;
+
+USE db_ElkGrove;
+
+CREATE TABLE USERS(
+	user_id			INT				NOT NULL	IDENTITY(1,1) ,
+	first_name		VARCHAR(50)		NOT NULL,
+	last_name		VARCHAR(50)		NOT NULL,
+	age				INT				NOT NULL,
+	street			VARCHAR(50)		NOT NULL,
+	city			VARCHAR(50)		NOT NULL,
+	state			VARCHAR(50)		NOT NULL,
+	zipcode			INT				NOT NULL,
+	PRIMARY KEY(user_id)
+);
+
+CREATE TABLE FOODS_PICTURE (
+	food_id			INT				NOT NULL	IDENTITY(1,1),
+	image_link		VARCHAR(50)		NOT NULL,
+	food_name		VARCHAR(50)		NOT NULL,
+	user_id			INT				NOT NULL,			
+	PRIMARY KEY(food_id),
+	FOREIGN KEY (user_id) REFERENCES USERS(user_id),
+);
+
+CREATE TABLE FOOD_LOCATION (
+	location_id		INT				NOT NULL	IDENTITY(1,1),
+	street			VARCHAR(50)		NOT NULL,
+	city			VARCHAR(50)		NOT NULL,
+	state			VARCHAR(50)		NOT NULL,
+	zipcode			INT				NOT NULL,
+	food_id			INT				NOT NULL,
+	PRIMARY KEY (location_id),
+	FOREIGN KEY (food_id) REFERENCES FOODS_PICTURE(food_id),
+);
+
+
+
---
+++ db_seeds.sql
@@ -0,0 +1,19 @@
+USE db_ElkGrove;
+
+INSERT INTO USERS (first_name, last_name, age, street, city, state, zipcode)
+VALUES 
+    ('Donald', 'Trump', 70, '3222 Hiker Way', 'Sacramento', 'California', 95888),
+    ('Helen', 'Trump', 60, '1231 Hee Way', 'New York', 'California', 12354),
+    ('Roger', 'Trump', 50, '5251 Haa Way', 'Elk Grove', 'California', 44112);
+
+INSERT INTO FOODS_PICTURE( image_link, food_name, user_id)
+VALUES 
+    ('https://picture.com', 'adobo', 1),
+    ('https://picture1.com', 'chikcen', 2),
+    ('https://picture2.com', 'beef', 3);
+
+INSERT INTO FOOD_LOCATION (street, city, state, zipcode, food_id)
+VALUES 
+    ('3111 Hel Way', 'Sacramento', 'California', 95758, 1),
+    ('1313 Hee Way', 'Folsom', 'California', 44112, 2),
+    ('3131 Haa Way', 'Elk Grove', 'California', 44112, 3);
